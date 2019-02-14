185e2e1594bc9d33c12cd213a62203d913a715c6
redaktorscha <39005857+redaktorscha@users.noreply.github.com>
2019-02-15 02:43:45 +0300

    styles fixed

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
-//dynamically add background-image to selected div's
+import {letterFade} from './letterfade.js';
+//import {changeBack} from './changeback';
 
-const changeBack = (picList) => {
-   
-    const urlArr = ["url('https://womenbz.ru/static/adv_quote/promo_quote/64.jpg')",
-    "url('https://womenbz.ru/images/quotes/mne-ochen-hotelos-takzhe.png')",
-    "url('https://womenbz.ru/images/quotes/kak-sdvinutsya-s-mertvoy-tochki.png')"];
-    let n = 0;
 
-    for (let i = 0; i < picList.length; i++) {
-        picList[i].style.backgroundImage = urlArr[n];
-        n++;
-    }
-}
 
-changeBack(document.querySelectorAll('.backgr-js'));
+//changeBack(document.querySelectorAll('.backgr-js'));
+letterFade(document.querySelectorAll('.blur-js'));
